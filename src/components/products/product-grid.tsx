be909830d2224dffe3ed0d4d4a22bbff85cd7e68
Ashish Construction <ashishconstruction1.co@gmail.com>
2025-10-04 20:19:07 +0000

    3 product in row

--- src/components/products/product-grid.tsx
+++ src/components/products/product-grid.tsx
@@ -5,7 +5,7 @@ export default async function ProductGrid() {
   const products = await getProducts();
 
   return (
-    <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+    <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 xl:gap-x-8">
       {products.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
