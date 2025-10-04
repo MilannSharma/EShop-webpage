b772e178a27c6728e855b348412f9c72ec663a75
Ashish Construction <ashishconstruction1.co@gmail.com>
2025-10-04 19:47:45 +0000

    i want position of l c same like shown in image

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -37,8 +37,8 @@ function Logo() {
         L
       </text>
       <text 
-        x="27.5" 
-        y="32.5" 
+        x="20" 
+        y="45" 
         fontFamily="Playfair Display, serif" 
         fontSize="36" 
         fontWeight="bold"
