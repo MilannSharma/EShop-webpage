2f88855750a13d3fcc3675b985d96514e7b8a5ef
Ashish Construction <ashishconstruction1.co@gmail.com>
2025-10-04 22:24:17 +0000

    remove logo ,

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -19,7 +19,6 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { navItems } from '@/lib/nav-data';
-import { Logo } from '../logo';
 
 
 export function Header() {
@@ -38,7 +37,7 @@ export function Header() {
               <SheetContent side="left" className="w-full max-w-sm overflow-y-auto">
                 <div className="flex flex-col gap-6 pt-8">
                   <Link href="/" className="flex items-center gap-2" onClick={() => setIsMobileMenuOpen(false)}>
-                    <Logo />
+                    
                     <span className="font-bold font-headline text-lg text-secondary-foreground tracking-wider">Lakshita Creations</span>
                   </Link>
                   <nav className="flex flex-col gap-4 text-lg font-medium">
@@ -92,7 +91,7 @@ export function Header() {
               </SheetContent>
             </Sheet>
           <Link href="/" className="flex items-center gap-2 ml-2 md:ml-0">
-            <Logo />
+            
             <span className="hidden md:inline font-bold font-headline text-lg text-foreground tracking-wider">Lakshita Creations</span>
           </Link>
         </div>
