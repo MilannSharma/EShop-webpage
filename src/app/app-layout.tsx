c5f5282b9df2c0262ec09905ddc88db7c8e2d0d8
Ashish Construction <ashishconstruction1.co@gmail.com>
2025-10-06 16:37:32 +0000

    i ant sidebar will collaps on the page only i dont want page should be r

--- src/app/app-layout.tsx
+++ src/app/app-layout.tsx
@@ -11,9 +11,9 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <div className="flex flex-1 overflow-hidden">
+      <div className="flex flex-1 overflow-hidden relative">
         <Sidebar />
-        <div className="flex-1 overflow-y-auto">
+        <div className="flex-1 overflow-y-auto md:pl-20">
           <MainContent>
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
               {children}
